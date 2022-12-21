71137a7b68e2b391da0e4b45081c670c76711cd4
McCoy Didericksen <mccoy.did@gmail.com>
2022-12-21 13:03:32 -0700

    setup basic javascript and prompts

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,29 +2,19 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
-    // Prompt user for password length;
-    var passwordLength = prompt("How many characters would you like your password to be?");
-    // If password length is less than 8 or greater than 128, alert user and return to beginning of function
-    if (passwordLength < 8 || passwordLength > 128) {
-      alert("Password length must be between 8 and 128 characters.");
-      return generatePassword();
-    }
-    // Prompt user for character types
-    var passwordLowercase = confirm("Would you like to include lowercase letters?");
-    var passwordUppercase = confirm("Would you like to include uppercase letters?");
-    var passwordNumeric = confirm("Would you like to include numbers?");
-    var passwordSpecial = confirm("Would you like to include special characters?");
+
 }
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+function startGame() {
+    // Prompt user for password length;
+    var userChoice = prompt("Enter r, p, or s to play the game.");
+    console.log(userChoice)
+    var choices = ["r", "p", "s"];
+    var computerChoice =  choices[Math.floor(Math.random() * choices.length)];
+    console.log(computerChoice)
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", startGame);
 
