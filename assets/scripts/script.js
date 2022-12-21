4b20c1e92b14e74932ecea809d7ca53ce3fd20f2
JCSH <joshmatsbux@gmail.com>
2022-12-21 10:19:19 -1000

    added decision logic and rules of play

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -5,13 +5,37 @@ function generatePassword() {
 
 }
 
-// Write password to the #password input
+// Game Function
 function startGame() {
-    // Prompt user for password length;
+    // Prompt user for game choice
     var userChoice = prompt("Enter r, p, or s to play the game.");
     console.log(userChoice)
     var choices = ["r", "p", "s"];
     var computerChoice =  choices[Math.floor(Math.random() * choices.length)];
+    var decision= ""
+    alert("The computer chose " + computerChoice)
+    if (userChoice === computerChoice){
+      alert("tie")
+    }
+      else if(userChoice="r" && computerChoice=="p"){
+        alert("Computer Wins")
+      }
+      else if(userChoice="p" && computerChoice=="r"){
+        alert("User Wins")
+      }
+      else if(userChoice="s" && computerChoice=="p"){
+        alert("User Wins")
+      }
+      else if(userChoice="r" && computerChoice=="s"){
+        alert("User Wins")
+      }
+      else if(userChoice="p" && computerChoice=="s"){
+        alert("Computer Wins")
+      }
+      else if(userChoice="s" && computerChoice=="r"){
+        alert("Computer Wins")
+      }
+    confirm("Would You like to play again")
     console.log(computerChoice)
 }
 
