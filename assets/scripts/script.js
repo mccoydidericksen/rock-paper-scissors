ad506cff9a264472ca8fc442c0ee5a237cd13111
McCoy Didericksen <mccoy.did@gmail.com>
2022-12-21 13:39:05 -0700

    add stats and alerts if user didn't enter in correct input

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,42 +1,54 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
-function generatePassword() {
-
-}
+var tieCount = 0
+var winCount = 0
+var lossCount = 0
 
 // Game Function
 function startGame() {
     // Prompt user for game choice
     var userChoice = prompt("Enter r, p, or s to play the game.");
-    console.log(userChoice)
     var choices = ["r", "p", "s"];
+    if (!choices.includes(userChoice)) {
+      alert("Please only enter r, p, or s")
+      startGame()
+    }
     var computerChoice =  choices[Math.floor(Math.random() * choices.length)];
-    var decision= ""
     alert("The computer chose " + computerChoice)
     if (userChoice === computerChoice){
       alert("tie")
+      tieCount += 1
     }
-      else if(userChoice="r" && computerChoice=="p"){
+      else if(userChoice==="r" && computerChoice==="p"){
         alert("Computer Wins")
+        lossCount += 1
       }
-      else if(userChoice="p" && computerChoice=="r"){
+      else if(userChoice==="p" && computerChoice==="r"){
         alert("User Wins")
+        winCount += 1
       }
-      else if(userChoice="s" && computerChoice=="p"){
+      else if(userChoice==="s" && computerChoice==="p"){
         alert("User Wins")
+        winCount += 1
       }
-      else if(userChoice="r" && computerChoice=="s"){
+      else if(userChoice==="r" && computerChoice==="s"){
         alert("User Wins")
+        winCount += 1
       }
-      else if(userChoice="p" && computerChoice=="s"){
+      else if(userChoice==="p" && computerChoice==="s"){
         alert("Computer Wins")
+        lossCount += 1
       }
-      else if(userChoice="s" && computerChoice=="r"){
+      else if(userChoice==="s" && computerChoice==="r"){
         alert("Computer Wins")
+        lossCount += 1
       }
-    confirm("Would You like to play again")
-    console.log(computerChoice)
+    
+    var playAgain = confirm("Wins: " + winCount + "\nLosses: " + lossCount + "\nTies: " + tieCount + "\nWould you like to play again?")
+    if (playAgain) {
+      startGame()
+    }
+ 
 }
 
 // Add event listener to generate button
